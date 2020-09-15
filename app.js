9dc33f430c0caba9c61ab277aa4b70a515223633
Brett Warnebold <68746756+perseusdevelopment@users.noreply.github.com>
2020-09-15 11:13:39 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+var campgrounds = [
+    { name: "Salmon Creek", image: "https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60" },
+    { name: "Mountain View", image: "https://images.unsplash.com/photo-1537225228614-56cc3556d7ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60" },
+    { name: "Lockheed Park", image: "https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60" },
+    { name: "Tesla Junction", image: "https://images.unsplash.com/photo-1571863533956-01c88e79957e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" },
+    { name: "Camp Crystal Lake", image: "https://images.unsplash.com/photo-1598128251172-b28ac1a9f63c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60" },
+    { name: "Torrey Pines", image: "https://images.unsplash.com/photo-1578145288677-6e6842916b90?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60" },
+    { name: "Haag Pass", image: "https://images.unsplash.com/photo-1533873984035-25970ab07461?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=60" }
+];
+
 app.get("/", function(req, res){
-    res.render("landingpage")
+    res.render("landingpage");
 });
 
 app.get("/campgrounds", function(req, res){
-    var campgrounds = [
-        { name: "Salmon Creek", image:"https://www.photosforclass.com/download/px_1061640"},
-        { name: "Mountain View", image:"https://www.photosforclass.com/download/px_2108709"},
-        { name: "Lockheed Park", image:"https://www.photosforclass.com/download/pb_5441144"},
-        { name: "Tesla Junction", image:"https://www.photosforclass.com/download/pb_4817872"}
-    ]
 
     res.render("campgrounds", {campgrounds: campgrounds});
 
 });
 
-app.post("/campgrounds")
+app.get("/campgrounds/new", function(req, res){
+    res.render("new.ejs");
+});
 
-app.listen(3000, function () {
+app.post("/campgrounds", function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image}
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
+});
+
+app.listen(3000, function(){
     console.log("YelpCamp Server is a GO");
 });
